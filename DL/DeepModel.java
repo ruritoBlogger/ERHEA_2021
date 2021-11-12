b340ebb5171d8ada0bc2d23744616911c63f076e
ruritoblogger <rurito.blog@gmail.com>
2021-11-12 18:32:25 +0900

    add td(lambda)

--- DL/DeepModel.java
+++ DL/DeepModel.java
@@ -1,4 +1,5 @@
 package DL;
+
 import java.io.File;
 
 import org.deeplearning4j.nn.conf.layers.*;
@@ -18,7 +19,6 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.learning.config.Adam;
 
-
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import org.deeplearning4j.nn.conf.GradientNormalization;
@@ -30,227 +30,245 @@ import enumerate.Action;
 import java.util.*;
 
 public class DeepModel {
-   static private MultiLayerNetwork net;
-   static private int seed = 123;  // seed is 123;
+	static private MultiLayerNetwork net;
+	static private int seed = 123; // seed is 123;
 
-   static private double learningRate = 0.001;
+	static private double learningRate = 0.001;
 
-   static private HashMap<Action, Integer> actToIndex;
-   final static private int numOutputs = 56;
-   final static private int n_epochs = 3; 
+	static private HashMap<Action, Integer> actToIndex;
+	final static private int numOutputs = 56;
+	final static private int n_epochs = 3;
 
-   final static private String directory = "./data/aiData/ERHEA_PPO_PG";
-   private static String oppName;
-   private static String myName;
-   private static String oppModelName;
-  
+	final static private String directory = "./data/aiData/ERHEA_PPO_PG";
+	private static String oppName;
+	private static String myName;
+	private static String oppModelName;
 
-   private static String lossType;
+	private static String lossType;
 	private String character;
 
+	public DeepModel(int input_size, String myName, String oppName, String lossType, String character) {
+		this.myName = myName;
+		this.oppName = oppName;
+		this.lossType = lossType;
+		this.character = character;
+		this.oppModelName = "oppModel.zip";
+		this.oppModelName = this.lossType + "-" + this.oppModelName;
+		// network learning model
+		System.out.println("Create " + this.lossType + " Model!");
 
-	public DeepModel(int input_size, String myName, String oppName, String lossType, String character){
-   	      this.myName = myName;
-   	      this.oppName = oppName;
-          this.lossType = lossType;
-          this.character = character;
-          this.oppModelName = "oppModel.zip";
-          this.oppModelName = this.lossType + "-" + this.oppModelName;
-          // network learning model
-          System.out.println("Create " + this.lossType + " Model!");
-           
+		MultiLayerConfiguration conf = getConfig(input_size);
+		net = new MultiLayerNetwork(conf);
+		net.init();
+		net.setListeners(new ScoreIterationListener(1));
 
-		   MultiLayerConfiguration conf = getConfig(input_size);
-		   net = new MultiLayerNetwork(conf);
-		   net.init();
-		   net.setListeners(new ScoreIterationListener(1));
+		actToIndex = new HashMap<Action, Integer>();
+		for (int i = 0; i < Action.values().length; i++) {
+			actToIndex.put(Action.values()[i], i);
+		}
 
-		   actToIndex = new HashMap<Action, Integer>();
-		   for (int i=0; i< Action.values().length; i++){
-		   	  actToIndex.put(Action.values()[i],i);
-		   }
+	}
 
-   }
-   
+	// num input: my HP, my Energy, my Posx, my Posy, my state, diff posx, opp HP,
+	// opp Energy, opp Posx, opp Posy, opp State,
+	// my Last Act,opp Last Act
+	private MultiLayerConfiguration getConfig(int input_size) {
 
-   
-// num input: my HP, my Energy, my Posx, my Posy, my state, diff posx, opp HP, opp Energy, opp Posx, opp Posy, opp State, 
-           // my Last Act,opp Last Act
-   private MultiLayerConfiguration getConfig(int input_size){
-	   
-	   if (lossType.toLowerCase()== "pi"){
-		   return new NeuralNetConfiguration.Builder()
-				   .seed(seed)
-//				   .weightInit(WeightInit.XAVIER)
-				   .weightInit(WeightInit.ONES)
-				   .biasInit(0.0)
-				   .gradientNormalization(GradientNormalization.ClipL2PerLayer)
-				   .gradientNormalizationThreshold(1.0)
-				   .updater(new Adam(learningRate, 0.99, 0.999, 0.00000001)) //
-				   .list()
-				   .layer(0,new OutputLayer.Builder(new PILoss())  // best performance: PILoss()
-						   .activation(Activation.SOFTMAX)   // pi: softmax, q: identity
-						   .nIn(input_size).nOut(numOutputs).build())
-				   .build();
+		if (lossType.toLowerCase() == "pi") {
+			return new NeuralNetConfiguration.Builder().seed(seed)
+					// .weightInit(WeightInit.XAVIER)
+					.weightInit(WeightInit.ONES).biasInit(0.0)
+					.gradientNormalization(GradientNormalization.ClipL2PerLayer).gradientNormalizationThreshold(1.0)
+					.updater(new Adam(learningRate, 0.99, 0.999, 0.00000001)) //
+					.list().layer(0, new OutputLayer.Builder(new PILoss()) // best performance: PILoss()
+							.activation(Activation.SOFTMAX) // pi: softmax, q: identity
+							.nIn(input_size).nOut(numOutputs).build())
+					.build();
 
-	   }
-	   else if (lossType.toLowerCase() == "sl"){
-		   return new NeuralNetConfiguration.Builder()
-				   .seed(seed)
-//				   .weightInit(WeightInit.XAVIER)
-				   .weightInit(WeightInit.ONES)
-				   .gradientNormalization(GradientNormalization.ClipL2PerLayer)
-				   .gradientNormalizationThreshold(1.0)
-				   .updater(new Adam(learningRate, 0.99, 0.999, 0.00000001)) //
-				   .list()
-				   .layer(0,new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT)  // best performance: PILoss()
-						   .activation(Activation.SOFTMAX)   // pi: softmax, q: identity
-						   .nIn(input_size).nOut(numOutputs).build())
-				   .build();
+		} else if (lossType.toLowerCase() == "sl") {
+			return new NeuralNetConfiguration.Builder().seed(seed)
+					// .weightInit(WeightInit.XAVIER)
+					.weightInit(WeightInit.ONES).gradientNormalization(GradientNormalization.ClipL2PerLayer)
+					.gradientNormalizationThreshold(1.0).updater(new Adam(learningRate, 0.99, 0.999, 0.00000001)) //
+					.list().layer(0, new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT) // best performance:
+																								// PILoss()
+							.activation(Activation.SOFTMAX) // pi: softmax, q: identity
+							.nIn(input_size).nOut(numOutputs).build())
+					.build();
 
-	    }
-      System.err.printf("Not right loss:%s \n", lossType);
-      return null;
+		}
+		System.err.printf("Not right loss:%s \n", lossType);
+		return null;
 
-   }
-   
+	}
 
-   
-   public void train(float[][] cur_input_info,  float[][] nx_input_info, float[] sel_actions, float[] opp_sel_actions, float win_signal){
-	   int batch_size = sel_actions.length;
+	public void train(float[][] cur_input_info, float[][] nx_input_info, float[] sel_actions, float[] opp_sel_actions,
+			float win_signal) {
+		long startTime = System.nanoTime();
+		int batch_size = sel_actions.length;
 
-	   INDArray sel_actsND = Nd4j.create(sel_actions, new int[]{batch_size, 1});
-	   INDArray opp_sel_actsND = Nd4j.create(opp_sel_actions, new int[]{batch_size, 1});
-	   INDArray cur_nd = Nd4j.create(cur_input_info);
-	   
-	     
+		INDArray sel_actsND = Nd4j.create(sel_actions, new int[] { batch_size, 1 });
+		INDArray opp_sel_actsND = Nd4j.create(opp_sel_actions, new int[] { batch_size, 1 });
+		INDArray cur_nd = Nd4j.create(cur_input_info);
 
-	   if (lossType == "sl"){
-	   	   int [] labels = new int[opp_sel_actions.length];
-	   	   for (int i=0; i<labels.length; i++)
-	   	   	   labels[i] = (int)(opp_sel_actions[i]);
-	   	   for (int i=0; i<n_epochs; i++){
-	   	   		net.fit(cur_nd, labels);
-	   	   		System.out.println("Loss:" + net.score());
-	   	   }
-	   }
-	   else{
-		   float discount = 0.99f;
-		   float [] targets_opp = new float[batch_size];
-		   float [] targets_sel = new float[batch_size];
-		   float reward_sel = 0;
-		   float reward_opp = 0;
-		   
-		   targets_opp[batch_size - 1] = win_signal; 
-		   targets_sel[batch_size - 1] = win_signal;
+		if (lossType == "sl") {
+			int[] labels = new int[opp_sel_actions.length];
+			for (int i = 0; i < labels.length; i++)
+				labels[i] = (int) (opp_sel_actions[i]);
+			for (int i = 0; i < n_epochs; i++) {
+				net.fit(cur_nd, labels);
+				System.out.println("Loss:" + net.score());
+			}
+		} else {
+			float discount = 0.99f;
+			float[] targets_opp = new float[batch_size];
+			float[] targets_sel = new float[batch_size];
+			float[] opp_dp = new float[batch_size + 1];
+			float[] sel_dp = new float[batch_size + 1];
+			float reward_sel = 0;
+			float reward_opp = 0;
 
-		   for (int i=batch_size-2; i>=0; i--){
-			   
-			   reward_sel = -Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f) - 0.01f;
-			   reward_opp = Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f) + 0.01f;
-			   
-//			   v1 -- 55.9
-//			   reward_sel = -Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f) + 0.01f;
-//			   reward_opp = Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f) - 0.01f;
-			   
-//			   // my player - soso-0
-//			   reward = Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f);
-			   
-//			   //   opp player - not so well
-//			   reward = Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f);
-			   
-//	            opp player    --not so well                                     our player
-//				reward = Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f) - Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f); 
-				   
+			float lambda = 0.8f;
+			int lambda_len = 3;
 
-			//  no well     
-//			   reward = Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f) + Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f); 
-			   
-			   
-			   
-			   // not well        
-//			   reward = Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f) - Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f); 
-			   
-			   
-			   
-			   //                    our player hp diff                                   opp player hp diff
-//			   reward = Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f) - Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f); 
-			   targets_opp[i] = reward_opp + discount * targets_opp[i+1];
-			   targets_sel[i] = reward_sel + discount * targets_sel[i+1];
-			  
-//			   targets_f[i] = reward;
-			 
-	
-			  
-			   
-		   }
+			float[] opp_lambda = new float[lambda_len];
+			float[] sel_lambda = new float[lambda_len];
 
-		   System.out.println("");
-		   
-//		   // Normalize reward part
-//		   for (int i=0; i<batch_size-1;i++){
-//			   targets_f[i] = targets_f[i]/ batch_size;
-//		   }
- 
-		     
-		   // target values
-		   INDArray target_sel = Nd4j.create(targets_sel, new int[] {batch_size, 1});
-		   INDArray target_opp = Nd4j.create(targets_opp, new int[] {batch_size, 1});
-	       
-		   // train repeatedly
-		   for (int i=0; i<n_epochs; i++){   
-				   net.setMask(sel_actsND);
-				   net.fit(cur_nd, target_sel);
-//				   System.out.println("Loss:" + net.score());
-				   
-				   net.setMask(opp_sel_actsND);
-				   net.fit(cur_nd, target_opp);
-		   }
+			targets_opp[batch_size - 1] = win_signal;
+			targets_sel[batch_size - 1] = win_signal;
+			opp_dp[0] = 0;
+			sel_dp[0] = 0;
 
-	   }
+			for (int i = batch_size - 2; i >= 0; i--) {
 
-   }
-	  
-	 
-	  
-	 	 
-   
-	public int forward(float[] input_info){  
-		int num_out=0;
+				reward_sel = -Math.max(nx_input_info[i][0] - nx_input_info[i + 1][0], 0.f) - 0.01f;
+				reward_opp = Math.max(nx_input_info[i][1] - nx_input_info[i + 1][1], 0.f) + 0.01f;
+
+				// v1 -- 55.9
+				// reward_sel = -Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f) +
+				// 0.01f;
+				// reward_opp = Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f) -
+				// 0.01f;
+
+				// // my player - soso-0
+				// reward = Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f);
+
+				// // opp player - not so well
+				// reward = Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f);
+
+				// opp player --not so well our player
+				// reward = Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f) -
+				// Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f);
+
+				// no well
+				// reward = Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f) +
+				// Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f);
+
+				// not well
+				// reward = Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f) -
+				// Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f);
+
+				// our player hp diff opp player hp diff
+				// reward = Math.max(nx_input_info[i][0] - nx_input_info[i+1][0], 0.f) -
+				// Math.max(nx_input_info[i][1] - nx_input_info[i+1][1], 0.f);
+				/*
+				 * targets_opp[i] = reward_opp + discount * targets_opp[i + 1]; targets_sel[i] =
+				 * reward_sel + discount * targets_sel[i + 1];
+				 */
+
+				// targets_opp[i] = reward_opp + discount * targets_opp[i + 1];
+				// targets_sel[i] = reward_sel + discount * targets_sel[i + 1];
+				opp_dp[batch_size - 1 - i] = reward_opp + opp_dp[batch_size - 2 - i];
+				sel_dp[batch_size - 1 - i] = reward_sel + sel_dp[batch_size - 2 - i];
+
+				opp_lambda[0] = reward_opp + discount * targets_opp[i + 1];
+				sel_lambda[0] = reward_sel + discount * targets_sel[i + 1];
+
+				if (i < batch_size - 2) {
+					opp_lambda[1] = opp_dp[batch_size - 1 - i] - opp_dp[batch_size - 3 - i]
+							+ discount * discount * targets_opp[i + 1];
+					sel_lambda[1] = sel_dp[batch_size - 1 - i] - sel_dp[batch_size - 3 - i]
+							+ discount * discount * targets_sel[i + 1];
+				} else {
+					opp_lambda[1] = 0;
+					sel_lambda[1] = 0;
+				}
+
+				if (i < batch_size - 3) {
+					opp_lambda[2] = opp_dp[batch_size - 1 - i] - opp_dp[batch_size - 4 - i]
+							+ discount * discount * discount * targets_opp[i + 1];
+					sel_lambda[2] = sel_dp[batch_size - 1 - i] - sel_dp[batch_size - 4 - i]
+							+ discount * discount * discount * targets_sel[i + 1];
+				} else {
+					opp_lambda[2] = 0;
+					sel_lambda[2] = 0;
+				}
+				for (int j = 0; j < lambda_len; j++) {
+					targets_opp[i] += opp_lambda[j] * Math.pow(lambda, j);
+					targets_sel[i] += sel_lambda[j] * Math.pow(lambda, j);
+				}
+				targets_opp[i] *= (1 - lambda);
+				targets_sel[i] *= (1 - lambda);
+			}
+
+			System.out.println("");
+
+			// // Normalize reward part
+			// for (int i=0; i<batch_size-1;i++){
+			// targets_f[i] = targets_f[i]/ batch_size;
+			// }
+
+			// target values
+			INDArray target_sel = Nd4j.create(targets_sel, new int[] { batch_size, 1 });
+			INDArray target_opp = Nd4j.create(targets_opp, new int[] { batch_size, 1 });
+
+			// train repeatedly
+			// for (int i = 0; i < n_epochs; i++) {
+			for (int i = 0; i < 2; i++) {
+				net.setMask(sel_actsND);
+				net.fit(cur_nd, target_sel);
+				// System.out.println("Loss:" + net.score());
+
+				net.setMask(opp_sel_actsND);
+				net.fit(cur_nd, target_opp);
+			}
+			System.out.println("処理時間: " + (System.nanoTime() - startTime) + "ナノ秒");
+		}
+
+	}
+
+	public int forward(float[] input_info) {
+		int num_out = 0;
 		INDArray input;
 		INDArray out;
 
 		input = Nd4j.create(input_info);
 		out = net.output(input);
-		
-	   
-	   INDArray argmax;
-	   //System.out.println("Out:" + out);
-	   argmax =  Nd4j.argMax(out, 1);  
-	   num_out = (int)(argmax.getInt(0));
-	   
 
-	  
-	   return num_out;
-   }
-	
-	
-	public int forward(float[] input_info, LinkedList<Action> validActs){
-		 
-		int num_out=0;
+		INDArray argmax;
+		// System.out.println("Out:" + out);
+		argmax = Nd4j.argMax(out, 1);
+		num_out = (int) (argmax.getInt(0));
+
+		return num_out;
+	}
+
+	public int forward(float[] input_info, LinkedList<Action> validActs) {
+
+		int num_out = 0;
 		double maxV = -99999999.0;
 		double val;
 		INDArray input;
 		INDArray out;
-		
+
 		input = Nd4j.create(input_info);
 		out = net.output(input);
-		
+
 		double[] ds = out.toDoubleVector();
-		for (Action act:validActs ){
+		for (Action act : validActs) {
 			int i = actToIndex.get(act);
 			val = ds[i];
-			if (val>maxV){
+			if (val > maxV) {
 				maxV = val;
 				num_out = i;
 			}
@@ -258,51 +276,47 @@ public class DeepModel {
 
 		return num_out;
 
-		
-   }
+	}
 
-   private String getPathName(){
-   	  String myname;
-   	  String oppname;
-   	  myname = this.myName == ""?  "": this.myName + "-";
-   	  oppname = this.oppName == ""? "": this.oppName + "-";
-   	  return this.directory + "/" + myname + oppname +  this.oppModelName + '-' + this.character;
-   }
-	
-   public void save(){
-       File file = new File(this.directory);
-       if (!file.exists()) { file.mkdir();}
+	private String getPathName() {
+		String myname;
+		String oppname;
+		myname = this.myName == "" ? "" : this.myName + "-";
+		oppname = this.oppName == "" ? "" : this.oppName + "-";
+		return this.directory + "/" + myname + oppname + this.oppModelName + '-' + this.character;
+	}
 
-	   String pathName;
-       pathName = getPathName();
-	   try{
-			   System.out.println("Save Model!");
-			   ModelSerializer.writeModel(net, new File(pathName), true); // false is not saved optimization parameters.
-	   }
-	   catch(Exception e){
-		   e.printStackTrace();
-	   }
-	   
-   }
-   
-   public void load(){
-   	   String pathName;
-   	   pathName = getPathName();
-	   try{
-		   File loadFile = new File(pathName);
-		   if(loadFile.exists()) {
-			   System.out.println("Loaded Model!");
-			   net = ModelSerializer.restoreMultiLayerNetwork(loadFile);
-		   }
-		   else{
-		   	   System.out.println("Could not find out the model!");
-		   }
-	   }
-	   catch(Exception e){
-		   e.printStackTrace();
-	   }
-   }
+	public void save() {
+		File file = new File(this.directory);
+		if (!file.exists()) {
+			file.mkdir();
+		}
 
+		String pathName;
+		pathName = getPathName();
+		try {
+			System.out.println("Save Model!");
+			ModelSerializer.writeModel(net, new File(pathName), true); // false is not saved optimization parameters.
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
+	}
+
+	public void load() {
+		String pathName;
+		pathName = getPathName();
+		try {
+			File loadFile = new File(pathName);
+			if (loadFile.exists()) {
+				System.out.println("Loaded Model!");
+				net = ModelSerializer.restoreMultiLayerNetwork(loadFile);
+			} else {
+				System.out.println("Could not find out the model!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- ERHEA_PPO_PG.java
+++ ERHEA_PPO_PG.java
@@ -1,7 +1,6 @@
 import aiinterface.AIInterface;
 import aiinterface.CommandCenter;
 
-
 import enumValue.CharName;
 import enumValue.GameMode;
 import enumerate.Action;
@@ -16,65 +15,56 @@ import org.apache.log4j.BasicConfigurator;
 import AI.BaseAI;
 
 public class ERHEA_PPO_PG implements AIInterface {
-    private Key inputKey;
-    private boolean player;
-    private FrameData frameData;
-    private CommandCenter cc;
-    private GameData gd;
-   
-    // game Character Name
-    private CharName charName;
-    	
+	private Key inputKey;
+	private boolean player;
+	private FrameData frameData;
+	private CommandCenter cc;
+	private GameData gd;
+
+	// game Character Name
+	private CharName charName;
+
 	// RHEA AI
 	BaseAI ai;
-	
-	private float win1=0, win2=0;
-	private float win=0;
-	private int round = 0;
-	private int frozenFrames=0;
 
-	
+	private float win1 = 0, win2 = 0;
+	private float win = 0;
+	private int round = 0;
+	private int frozenFrames = 0;
+
 	// Set AI mode
-	public void setAIMode(){
+	public void setAIMode() {
 		System.out.println("Call First Get Information");
 		// CommandCenter c = new CommandCenter();
 		// c.setFrameData(frameData, player);
 
-		String lossType = "pi";    // pi, sl
-		boolean specificEnemy=true;
-
+		String lossType = "pi"; // pi, sl
+		boolean specificEnemy = true;
 
 		// Just Set Game Node hold on.
 		ai = new BaseAI(charName, gd, player, lossType, specificEnemy);
 
 		// load AI model
 		ai.gi.dlOpModel.loadModel();
-	
-	}
-	
 
-	
+	}
+
 	@Override
 	public void close() {
 		System.gc();
 		System.out.println("Close AI!");
 	}
-	
-	
-	
-
 
 	@Override
 	public void getInformation(FrameData fd, boolean control) {
-		 this.frameData = fd;
-		 ai.control = control;
-		 ai.getInformation(this.frameData);
-		 this.cc.setFrameData(frameData, this.player);
-		 ai.gi.isEmcmAiMaxFlag = false;
-		 ai.gi.isUseEmcmAiMaxFlag = false;
-		 timeFlag = true;
-		 
-		 
+		this.frameData = fd;
+		ai.control = control;
+		ai.getInformation(this.frameData);
+		this.cc.setFrameData(frameData, this.player);
+		ai.gi.isEmcmAiMaxFlag = false;
+		ai.gi.isUseEmcmAiMaxFlag = false;
+		timeFlag = true;
+
 	}
 
 	@Override
@@ -87,16 +77,19 @@ public class ERHEA_PPO_PG implements AIInterface {
 		this.cc = new CommandCenter();
 		this.gd = gd;
 		String s_charName = this.gd.getCharacterName(this.player);
-		if (s_charName.equals("ZEN")) charName = CharName.ZEN;
-		else if (s_charName.equals("GARNET")) charName = CharName.GARNET;
-		else charName = CharName.OTHER;
-		
+		if (s_charName.equals("ZEN"))
+			charName = CharName.ZEN;
+		else if (s_charName.equals("GARNET"))
+			charName = CharName.GARNET;
+		else
+			charName = CharName.OTHER;
+
 		// Frozen Frames
 		this.frozenFrames = 0;
-		
+
 		// Set AI Mode
 		setAIMode();
-		
+
 		return 0;
 	}
 
@@ -104,21 +97,17 @@ public class ERHEA_PPO_PG implements AIInterface {
 	public Key input() {
 		return inputKey;
 	}
-	
-	
-
 
 	@Override
 	public void processing() {
-		
+
 		// can process
-		if (!frameData.getEmptyFlag() && frameData.getRemainingFramesNumber()>0){
-			
+		if (!frameData.getEmptyFlag() && frameData.getRemainingFramesNumber() > 0) {
+
 			// keep do skill
-			if (cc.getSkillFlag()){
+			if (cc.getSkillFlag()) {
 				inputKey = cc.getSkillKey();
-			}
-			else{
+			} else {
 				inputKey.empty();
 				cc.skillCancel();
 				String key = ai.getDoAction();
@@ -136,20 +125,23 @@ public class ERHEA_PPO_PG implements AIInterface {
 				timeFlag = false;
 				long t = (System.nanoTime() - ai.start);
 				if (t > 16500000) {
-					delayTime ++;
+					delayTime++;
 					avgTime += t;
-//					System.out.println("remain time of ERHEA:" + new Double((t) / 1000).toString());
+					// System.out.println("remain time of ERHEA:" + new Double((t) /
+					// 1000).toString());
 				}
-				allTime ++;
+				allTime++;
 				ai.gi.AllEvolveTimes += ai.gi.evolveTimes;
 			}
 		}
 	}
+
 	long avgTime = 0;
 	int delayTime = 0;
 	int allTime = 0;
-//	int processingTime = 0;
+	// int processingTime = 0;
 	boolean timeFlag = true;
+
 	@Override
 	public void roundEnd(int p1_hp, int p2_hp, int frames) {
 		System.out.println("ERHEA_PI_DJL avgtime: " + new Long(avgTime / (delayTime + 1)).toString());
@@ -157,6 +149,7 @@ public class ERHEA_PPO_PG implements AIInterface {
 		System.out.println("ERHEA_PI_DJL time size: " + allTime);
 		System.out.println("ERHEA_PI_DJL ERHEA size: " + ai.gi.processingTime);
 		System.out.println("ERHEA_PI_DJL avg ai.gi.evolveTimes: " + ai.gi.AllEvolveTimes / (ai.gi.processingTime + 1));
+		long startTime = System.nanoTime();
 		ai.gi.AllEvolveTimes = 0;
 		allTime = 0;
 		delayTime = 0;
@@ -164,66 +157,63 @@ public class ERHEA_PPO_PG implements AIInterface {
 		ai.gi.processingTime = 0;
 
 		round = round + 1;
-        System.out.println("Round End!");
+		System.out.println("Round End!");
 		float win_signal = 1;
-		if (this.player && p1_hp < p2_hp){
+		if (this.player && p1_hp < p2_hp) {
 			win_signal = -1;
-		}
-		else if (!this.player && p1_hp > p2_hp){
+		} else if (!this.player && p1_hp > p2_hp) {
 			win_signal = -1;
 		}
 		// 暴力切换器 zhongqian TODO 0720
 		if (ai.SpeedModeFlag == false && win_signal == -1) {
 			ai.use_ppo = !ai.use_ppo;
 		}
-        
-        // train it.
-        ai.gi.dlOpModel.train_batch(win_signal);
-        ai.gi.dlOpModel.saveModel();
+
+		// train it.
+		ai.gi.dlOpModel.train_batch(win_signal);
+		ai.gi.dlOpModel.saveModel();
 
 		// reset info
 		ai.gi.resetInfo();
-		
+
 		// print statistics
-		win1 = p1_hp > p2_hp ? win1+1 : win1;
-		win2 = p1_hp < p2_hp ? win2+1 : win2;
-		
+		win1 = p1_hp > p2_hp ? win1 + 1 : win1;
+		win2 = p1_hp < p2_hp ? win2 + 1 : win2;
+
 		win = win1 + win2;
-		if (win !=0){
-			win = this.player? win1 / win: win2 / win;
+		if (win != 0) {
+			win = this.player ? win1 / win : win2 / win;
 		}
-		String winstr=("Round:\t" + round +"\t" + (win_signal>0?"WIN":"LOSE"));
-		System.err.println("I'm:\t"+this.gd.getAiName(this.player));
-		System.err.println("Opp:\t"+this.gd.getAiName(!this.player));
-		System.err.println("Chara:\t"+this.gd.getCharacterName(this.player));
-		System.err.println(winstr+"\twinrate\t"+win);
+		String winstr = ("Round:\t" + round + "\t" + (win_signal > 0 ? "WIN" : "LOSE"));
+		System.err.println("I'm:\t" + this.gd.getAiName(this.player));
+		System.err.println("Opp:\t" + this.gd.getAiName(!this.player));
+		System.err.println("Chara:\t" + this.gd.getCharacterName(this.player));
+		System.err.println(winstr + "\twinrate\t" + win);
 
 		boolean iswim = (this.player && p1_hp > p2_hp && p2_hp == 0) || (!this.player && p1_hp < p2_hp && p1_hp == 0);
 		frames = (iswim) ? frames : 4200;
 		System.err.println("Framenumber: " + frames);
 
 		System.err.flush();
-		
+
 		// round end clean
 		this.inputKey.empty();
 		this.cc.skillCancel();
+		System.out.println("全体の処理時間: " + (System.nanoTime() - startTime) + "ナノ秒");
+
 		System.gc();
-		
-		
-        
+
 	}
 
-    	// modify by init.sh
-    @Override
-    public void getInformation(FrameData frameData) {
+	// modify by init.sh
+	@Override
+	public void getInformation(FrameData frameData) {
 
-    }
+	}
 
-    @Override
-    public void getScreenData(ScreenData sd) {
-        AIInterface.super.getScreenData(sd);
-    }
-
-
+	@Override
+	public void getScreenData(ScreenData sd) {
+		AIInterface.super.getScreenData(sd);
+	}
 
 }
